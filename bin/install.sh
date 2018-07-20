b3f178094ab299cd8992b0d8b9b7d1c07cba9237
Sandor Farkas <sandor.farkas@gmail.com>
2018-07-20 13:19:15 +0200

    Changing concept of copying environment profile files

--- bin/install.sh
+++ bin/install.sh
@@ -3,7 +3,6 @@
 # if directory name contains _ it will be used as environment suffix
 declare -a profile_dirs=(
   "${HOME}/workspace/lab/dotfiles/profiles"
-  #"${HOME}/workspace/.dotfiles/lab/profiles_td"
 ) 
 
 # @param $1 dir
@@ -21,32 +20,17 @@ link_file () {
 
 # @param $1 directory of profile files
 linking_profiles_from_directory () {
-  FILES=$(find $1 -maxdepth 1 -type f -printf "%f\n")
-  for filename in $FILES; do
-    link_file $1 $filename $2
+  files=$(find $1 -maxdepth 1 -type f -printf "%f\n")
+  for filename in $files; do
+    link_file $1 $filename
   done
-}
-
-# @param $1 dir
-copy_profile_files () {
-  suffix=$(echo $1 | cut -d'_' -f 2)
-  echo "suffix ${suffix}"
-  # todo
 }
 
 loop_profile_dirs () {
   for profile_dir in ${profile_dirs[@]}; do
     if [ -d $profile_dir ]; then
-
-      if [[ $profile_dir = *"_"* ]]; then
-        echo "Copying profile files from ${profile_dir}"
-        copy_profile_files $profile_dir
-      else
-        echo "Linking profiles from directory ${profile_dir}"
-        linking_profiles_from_directory "${profile_dir}"
-      fi
-      
-      echo
+      echo "Linking profiles from directory ${profile_dir}"
+      linking_profiles_from_directory "${profile_dir}"
     fi
   done
 }

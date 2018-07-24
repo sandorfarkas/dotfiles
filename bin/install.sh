816ad116e6284acaf1b3a31aaba49204953624fe
sandorfarkas <sandor.farkas@gmail.com>
2018-07-24 23:12:12 +0200

    Saving backup to backup dir

--- bin/install.sh
+++ bin/install.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
+dotfiles="${HOME}/workspace/dotfiles"
+if [ ! -d "${dotfiles}/backup" ]; then mkdir "${dotfiles}/backup"; fi
 
 # if directory name contains _ it will be used as environment suffix
 declare -a profile_dirs=(
-  "${HOME}/workspace/lab/dotfiles/profiles"
+  "${dotfiles}/profiles"
 ) 
+
+# @param $1 filename
+backup_file () {
+  filename=$1
+ 
+  if [ -e "${HOME}/${filename}" ]; then
+    echo "Backing up ${HOME}/${filename} to ${dotfiles}/backup/${filename}.old"
+    mv "${HOME}/${filename}" "${dotfiles}/backup/${filename}.old"
+  fi
+}
 
 # @param $1 dir
 # @param $2 filename
 link_file () {
-  target_filename="${1}/${2}"
-  link_filename="${HOME}/${2}"
-  
-  if [ -f $link_filename ]; then
-    echo "Backing up ${link_filename} to ${link_filename}.old"
-    mv "${link_filename}" "${link_filename}.old"
-  fi
+  dir=$1
+  filename=$2
+
+  target_filename="${dir}/${filename}"
+  link_filename="${HOME}/${filename}"
+
+  backup_file "${filename}"
+
   ln -s "${target_filename}" "${link_filename}" 
 }
 
 # @param $1 directory of profile files
 linking_profiles_from_directory () {
-  files=$(find $1 -maxdepth 1 -type f -printf "%f\n")
+  profile_dir=$1
+  echo "Linking profiles from directory ${profile_dir}"
+
+  files=$(find $profile_dir -maxdepth 1 -type f -printf "%f\n")
   for filename in $files; do
-    link_file $1 $filename
+    link_file $profile_dir $filename
   done
 }
 
 loop_profile_dirs () {
   for profile_dir in ${profile_dirs[@]}; do
     if [ -d $profile_dir ]; then
-      echo "Linking profiles from directory ${profile_dir}"
       linking_profiles_from_directory "${profile_dir}"
     fi
   done
